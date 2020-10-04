803ec4539660e822d79f1ddba8a9f8e6fcf5843e
JKEOR <31055463+JKEOR@users.noreply.github.com>
2020-10-04 22:45:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -28,15 +28,25 @@ def main():
     score.get_page()
     grades = score.get_data()
 
-    # 计算绩点
-    year = '2019-2020'
-    get_gpa = Gpa(grades, year)
-    gpa = get_gpa.count_gpa()
-
-    # 输出信息
+    # 打印成绩
     print('\n', xm)
-    print(year + "学年绩点1(除去公共选修课)：" + gpa[0])
-    print(year + "学年绩点2(除去所有选修课)：" + gpa[1])
+    for grade in grades:
+        print(grade['学年'], grade['学期'], grade['课程名称'], '---', grade['成绩'])
+
+    # 获取有成绩的年份
+    years = []
+    for grade in grades:
+        years.append(grade['学年'])
+    years = list(set(years))
+
+    # 计算绩点
+    print('\n', xm)
+    for year in years:
+        get_gpa = Gpa(grades, year)
+        gpa = get_gpa.count_gpa()
+        print(year + "学年绩点1(除去公共选修课)：" + gpa[0])
+        print(year + "学年绩点2(除去所有选修课)：" + gpa[1], '\n')
+
     print("学位课绩点：" + gpa[2])
 
 
@@ -44,9 +54,8 @@ if __name__ == "__main__":
     msg = """此程序可以爬取沈阳理工大学教学网，实现自动计算绩点，查询成绩等功能。
 初次使用需要爬取教学计划，大概需要2-3分钟的时间，请耐心等待。
 初次运行会生成.json格式文件，该文件为学位课信息，请不要删除。
-更新地址:https://github.com/JKEOR/sylu_parser/releases
-version:1.1
-——————by JKOR"""
+开源地址:https://github.com/JKEOR/sylu_parser
+——————by JKOR\n"""
     print(msg)
     main()
     input()
